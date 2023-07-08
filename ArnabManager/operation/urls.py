a238dee6f80a28d5cbe8b882e12a401a6f2e97b3
mohamedSaber <mm@frfg.drfg>
2023-07-08 17:11:49 +0300

    local_css_and_js_bootstrap
    delete_is_ok
    
    update_for_arnaba_is_ok
    
    default_values_in_the_form_is_ok
    
    update_works

--- ArnabManager/operation/urls.py
+++ ArnabManager/operation/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('arnaba_create', views.arnaba_create, name='arnaba_create'),
     path('arnaba_list', views.arnaba_list, name='arnaba_list'),
+    path('update/<int:id>/', views.arnaba_update, name='arnaba_update'),
     path('delete/<int:id>/', views.arnaba_delete, name='arnaba_delete'),
     path('<int:id>/', views.detail, name='detail')
 
--- ArnabManager/operation/views.py
+++ ArnabManager/operation/views.py
@@ -5,6 +5,9 @@ from .models import *
 from operation.forms import *
 from django.core import serializers
 # Create your views here.
+### GLOBAL vars
+disabled_fields = ['id', 'birth_date']
+
 
 def index(request):
     return render(request, 'index.html')
@@ -31,7 +34,8 @@ def detail(request, id):
         context = {
             'arnab': arnab,
             'fields': fields,
-            'field_list': field_list
+            'field_list': field_list,
+            'disabled_fields': disabled_fields
 
         }
         return render(request, 'detail.html',context)
@@ -45,6 +49,7 @@ def arnaba_delete(request, id):
         return arnaba_list(request)
 
 ###### FORMS
+### ARANABA CREATE
 def arnaba_create(request):
     form = NewArnabaForm()
 
@@ -61,3 +66,31 @@ def arnaba_create(request):
         }
     return render(request, 'arnaba_create.html', context)
     
+
+
+##### ARNABA UPDATE
+def arnaba_update(request, id):
+    print (request.POST)
+    print (id)
+    arnab = Arnaba.objects.get(pk=id)
+    fields = arnab._meta.get_fields()
+    field_list = []
+    for field in fields:
+        if field.name not in disabled_fields:
+            setattr(arnab, field.name, request.POST[field.name])
+    arnab.save()
+
+    # form = NewArnabaForm()
+
+    # if request.method == "POST":
+    #     form = NewArnabaForm(request.POST)
+
+    #     if form.is_valid():
+    #            form.save(commit=True)
+    #            return arnaba_list(request)
+    #     else:
+    #            print('ERROR INVALID')
+    # context = {
+    #         'form': form
+    #     }
+    return render(request, 'index.html')
